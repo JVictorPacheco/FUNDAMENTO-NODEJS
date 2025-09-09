a207c4aeddf549e14a9a12cd2c4ddd5bbf762f3f
João Pacheco <95194054+JVictorPacheco@users.noreply.github.com>
2025-09-09 20:34:15 -0300

    Ajustando parametro do if

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 import http from 'node:http'
 
-
+// rodar npm run dev para executar o programa
 
 const server = http.createServer((request, response) => {
+    const { method, url } = request
+
+    if (method ===       'GET' && url =='/users') {
+            return response.end('Listagem de usuarios')
+    } 
+
+    if (method === 'POST' && url === '/users') {
+            return response.end('Crição de usuario')
+    } 
+
+    
 
     return response.end("Hello teste")
 })
