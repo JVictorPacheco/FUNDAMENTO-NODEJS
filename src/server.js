48b23ffd766eab3cb138af094ad388b2c83d0f1d
João Pacheco <95194054+JVictorPacheco@users.noreply.github.com>
2025-09-09 21:51:21 -0300

    Salvando usuario em memoria (Header)

--- src/server.js
+++ src/server.js
@@ -2,14 +2,26 @@ import http from 'node:http'
 
 // rodar npm run dev para executar o programa
 
+const users = []
+
 const server = http.createServer((request, response) => {
     const { method, url } = request
 
-    if (method ===       'GET' && url =='/users') {
-            return response.end('Listagem de usuarios')
+    if (method === 'GET' && url === '/users') {
+          return response
+                .setHeader('Content-type', 'applications/json')
+                .end(JSON.stringify(users)) //CONVERTENDO O TIPO DA VARIAVEL USER QUE E ARRAY PARA STRING
     } 
 
+
     if (method === 'POST' && url === '/users') {
+
+            users.push({
+                id: 1,
+                name: 'João pacheco',
+                email: 'vreds433@gmail.com'
+            })
+
             return response.end('Crição de usuario')
     } 
 
